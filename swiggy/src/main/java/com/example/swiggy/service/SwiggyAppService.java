f7e9013bf74b93bef27e18928aaf7c848f90a434
Vaibhav Rajput <vaibhavrajput9826@gmail.com>
2024-02-15 13:26:23 +0530

    first commit

---
+++ swiggy/src/main/java/com/example/swiggy/service/SwiggyAppService.java
@@ -0,0 +1,21 @@
+package com.example.swiggy.service;
+
+import com.example.swiggy.client.RestaurantServiceClient;
+import com.example.swiggy.dto.OrderResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SwiggyAppService {
+
+    @Autowired
+    private RestaurantServiceClient restaurantServiceClient;
+
+    public String greeting() {
+        return "Welcome to Swiggy App Service";
+    }
+
+    public OrderResponseDTO checkOrderStatus(String orderId) {
+        return restaurantServiceClient.fetchOrderStatus(orderId);
+    }
+}
